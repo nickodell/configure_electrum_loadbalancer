00443d176cdf2fef81d1895da440c7fe578051d1
Nick O'Dell <nickodell@gmail.com>
2015-05-03 17:31:09 -0600

    Automatically generated commit

---
+++ copy_to_target.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+git add .
+git commit -m "Automatically generated commit"
+git push
+ssh "$@" "rm -rf /root/configure_electrum_loadbalancer; \
+          apt-get install -y git && \
+          git clone https://github.com/nickodell/configure_electrum_loadbalancer && \
+          nohup ./configure_electrum_loadbalancer/configure.sh"
